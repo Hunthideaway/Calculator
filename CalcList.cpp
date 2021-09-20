492035cbb6bc7265d31b2bea69fc094f298ade1e
Raed56 <rseraj@usf.edu>
2021-09-19 21:16:21 -0400

    updated code

--- CalcList.cpp
+++ CalcList.cpp
@@ -1,136 +1,162 @@
+
 #include "CalcList.hpp"
 
 //constructor implementation
 
-CalcList:: CalcList(){
-    
+CalcList::CalcList()
+{
+
+    Calcnode* def = new Calcnode; //creating default node 
+
     //creating header and trailer nodes
     header = new Calcnode;
-    trailer= new Calcnode;
+    trailer = new Calcnode;
+    // set defaults
+    def->previousValue = 0;
+    def->secondValue = 0;
+    def->current = 0;
+    def->operation = ADDITION;
 
+    // connect head and trailer to default node
+    def->previous = header;
+    def->next = trailer;
+    header->next = def;
+    trailer->previous = def;
 
-    header->next=trailer; //making header point to trailer and vice versa
-    trailer->previous=header; 
-
+    // initialize total to zero
+    currentTotal = 0;
 }
 
 //destructor implementation
 
-CalcList:: ~CalcList(){
+CalcList::~CalcList()
+{
 
-    while (!checkEmpty()){//this loops continues as long as the list is not empty
-        delete(header->next);  //starts deleting from the node after header
+    Calcnode* node = header;
+    Calcnode* temp;
+
+    while (node != trailer)
+    {
+        temp = node->next;
+        delete node;
+        node = temp;
     }
-    //lastly delete header and trailer to realease all memory
-    delete header;
+
+    //lastly delete trailer to realease all memory
     delete trailer;
 }
 
-//check empty function implementation
-
-bool CalcList:: checkEmpty() const {
-    if (header->next==trailer){
-        return true;
-    }
-    return false;  //returns true if header and trailer pointing to each other, which means list is empty OR else returns false
+double CalcList::total() const
+{
+    return currentTotal; //returns the current total
 }
 
+void CalcList::newOperation(const FUNCTIONS func, const double operand)
+{
+    Calcnode* x = new Calcnode;      //create a new node, x
+    Calcnode* last = nullptr;        //pointer to node before the trailer
+    x->previousValue = currentTotal; //store the previous value, being the current total
+    x->current = operand;            //store the current value within operand for use within the operations
 
-
-double CalcList::total() const{
-   return currentTotal; //returns the current total 
-}
-
-
-void CalcList::newOperation(const FUNCTIONS func, const double operand){
-   
-    Calcnode* x = new Calcnode; //create a new node, x
-    Calcnode* last = nullptr; //pointer to node before the trailer 
-    x->previousValue = currentTotal; //store the previous value, being the current total 
-    x->current = operand; //store the current value within operand for use within the operations 
-
-    //identify which operation the user is attempting and then calculate it 
-    if(func == ADDITION) { //if the user enters addition 
-        currentTotal += operand; //add the current total and the user input together and store 
-        x->operation = ADDITION; //store the operation 
+    //identify which operation the user is attempting and then calculate it
+    if (func == ADDITION)
+    {                            //if the user enters addition
+        currentTotal += operand; //add the current total and the user input together and store
+        x->operation = ADDITION; //store the operation
     }
-    else if(func == SUBTRACTION) { //if the user enters subtraction 
-        currentTotal -= operand; //subtract the current total and the user input and then store them
-        x->operation = SUBTRACTION; //store the operation 
+    else if (func == SUBTRACTION)
+    {                               //if the user enters subtraction
+        currentTotal -= operand;    //subtract the current total and the user input and then store them
+        x->operation = SUBTRACTION; //store the operation
     }
-    else if(func == MULTIPLICATION) { //if the user enters mutliplication 
-        currentTotal *= operand; //multiply the current total by the user input 
-        x->operation = MULTIPLICATION; //store the operation 
+    else if (func == MULTIPLICATION)
+    {                                  //if the user enters mutliplication
+        currentTotal *= operand;       //multiply the current total by the user input
+        x->operation = MULTIPLICATION; //store the operation
     }
-    else if(func == DIVISION ) { //if the user enters division 
-        if (operand == 0 ) { //if the user enters 0 
-            delete x; //delete the value 
-            throw std::invalid_argument("You cannot divide by zero!!"); //inform the user that you cannot divide by 0 
-            return; //end the program 
+    else if (func == DIVISION)
+    { //if the user enters division
+        if (operand == 0)
+        {                                                               //if the user enters 0
+            delete x;                                                   //delete the value
+            throw std::invalid_argument("You cannot divide by zero!!"); //inform the user that you cannot divide by 0
+            return;                                                     //end the program
         }
-        else { //else if the number is not 0 
-            currentTotal /= operand; //divide the previous value by the current value 
-            x->operation = DIVISION; //store the operaton 
+        else
+        {                            //else if the number is not 0
+            currentTotal /= operand; //divide the previous value by the current value
+            x->operation = DIVISION; //store the operaton
         }
     }
     x->secondValue = currentTotal; //this updates the current total after the operation
-    last = trailer->previous; //storing the last node 
-    last->next = x; //links the last node to x 
-    x->previous = last; //links the previous node to last 
-    x->next = trailer; //links the next value to the trailer
-    trailer->previous = x; //links trailer to x 
-
-    numOperations++; //incrementing the step counter
-
+    last = trailer->previous;      //storing the last node
+    last->next = x;                //links the last node to x
+    x->previous = last;            //links the previous node to last
+    x->next = trailer;             //links the next value to the trailer
+    trailer->previous = x;         //links trailer to x
 }
 
-//undo feature of calculator
-void CalcList::removeLastOperation(){ 
-    numOperations--;
-    Calcnode* none = header; //variable that will be used if there is no operation 
-    Calcnode* previous = trailer->previous->previous; //this pointer references the node before the one that will be deleted 
-    Calcnode* nodeDelete = previous->next; //this pointer stores the node that will be deleted 
+void CalcList::removeLastOperation()
+{
 
-    if(none->next->next == trailer) { //if there are no operations that can be removed, do the following
+    Calcnode* none = header;                          //variable that will be used if there is no operation
+    Calcnode* previous = trailer->previous->previous; //this pointer references the node before the one that will be deleted
+    Calcnode* nodeDelete = previous->next;            //this pointer stores the node that will be deleted
+
+    if (none->next->next == trailer)
+    {                                                                      //if there are no operations that can be removed, do the following
         throw std::invalid_argument("No operation available to remove!!"); //inform the user that there is no operation that can be removed
-        return; //end the program 
+        return;                                                            //end the program
     }
-    else{
-    currentTotal = previous->secondValue; //this removes the last operation 
-    previous->next=trailer; //unlink the previous node 
-    trailer->previous = previous; //unlink the previous node 
-    delete nodeDelete; //delete the node 
-    }
-    numOperations--;
+        currentTotal = previous->secondValue; //this removes the last operation
+        previous->next = trailer;             //unlink the previous node
+        trailer->previous = previous;         //unlink the previous node
+        delete nodeDelete;                    //delete the node
 }
 
-std::string CalcList:: toString(unsigned short precision) const{
-    std::stringstream a; //declare the variable for the string 
+
+std::string CalcList::toString(unsigned short precision) const
+{
+
+    int count = 0; // number of nodes (doesn't count head, trailer or default node)
+
+    // count number of nodes
+    Calcnode* countNodes = header->next;
+    while (countNodes->next != trailer)
+    {
+
+        countNodes = countNodes->next;
+        count++;
+    }
+
+    std::stringstream a; //declare the variable for the string
 
     a.precision(precision); //
 
-    Calcnode* afterHeader = header->next; //pointer to the node after the header a
+    Calcnode* beforeEnd = trailer->previous; //pointer to the node before the trailer
 
-    int steps =numOperations; //holds the number of operation steps in the list
+    while (beforeEnd->previous != header)
+    { //as long as the node is not the header, do the following
 
-    while (afterHeader->next != trailer ) { //as long as the node is not the header, do the following 
-
-        if(afterHeader->operation == ADDITION ) { //if the users operation is addition, do the following 
-            a << steps << std::fixed << ":" << afterHeader->previousValue << "+" << afterHeader-> secondValue << std::endl;
+        if (beforeEnd->operation == ADDITION)
+        { //if the users operation is addition, do the following
+            a << count << std::fixed << ": " << beforeEnd->previousValue << "+" << beforeEnd->current << "=" << beforeEnd->secondValue << std::endl;
         }
-        else if(afterHeader->operation ==SUBTRACTION) { //if the users operation is addition, do the following
-            a<<  steps << std::fixed << ":" << afterHeader->previousValue << "-" << afterHeader->secondValue<<std::endl;
+        else if (beforeEnd->operation == SUBTRACTION)
+        { //if the users operation is addition, do the following
+            a << count << std::fixed << ": " << beforeEnd->previousValue << "-" << beforeEnd->current << "=" << beforeEnd->secondValue << std::endl;
         }
-        else if(afterHeader->operation == MULTIPLICATION) { //if the users operation is mitliplication do the following
-            a<< steps << std::fixed<<":"<<afterHeader->previousValue<<"*" <<afterHeader->secondValue<<std::endl; 
+        else if (beforeEnd->operation == MULTIPLICATION)
+        { //if the users operation is mitliplication do the following
+            a << count << std::fixed << ": " << beforeEnd->previousValue << "*" << beforeEnd->current << "=" << beforeEnd->secondValue << std::endl;
         }
-        else if(afterHeader->operation == DIVISION) { //if the users operation is division do the following
-            a<< steps << std::fixed<<":" << afterHeader->previousValue <<"/" << afterHeader->secondValue<<std::endl;
+        else if (beforeEnd->operation == DIVISION)
+        { //if the users operation is division do the following
+            a << count << std::fixed << ": " << beforeEnd->previousValue << "/" << beforeEnd->current << "=" << beforeEnd->secondValue << std::endl;
         }
 
-        afterHeader = afterHeader->next; //after the first operation, move onto the next one
-        --steps; //after each node values are printed, the steps variable is decremented 
-
+        count--;
+        beforeEnd = beforeEnd->previous; //after the first operation, move onto the next one
     }
     return a.str(); //return the string
 }
